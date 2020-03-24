1371202c479052de39f896bb90f51cb69dfe4f73
Brett Holland <brett.holland@gmail.com>
2020-03-24 16:56:07 -0400

    Install styled components and organize initial CRA files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/reset.css';
+import './styles/base.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
